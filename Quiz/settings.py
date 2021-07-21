331bcf767256fcf6bfab5d6086a57a091d350542
Siddhant Maurya <mauryasiddhant758@gmail.com>
2021-07-21 21:34:00 +0530

    first commit

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
+#import django_heroku
 
 import os
 
@@ -134,4 +134,4 @@ STATICFILES_DIRS=[
 #MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
