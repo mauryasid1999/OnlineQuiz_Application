1af8ab2c2c066b48b96e8b4a6f675f50c8bd7739
Siddhant Maurya <mauryasiddhant758@gmail.com>
2021-07-21 20:50:17 +0530

    first commit

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -123,12 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL='/media/'
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,'static'),
-)
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-
+#MEDIA_URL='/media/'
 STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 
-django_heroku.settings(locals())
+#STATICFILES_DIRS=[
+ #   os.path.join(BASE_DIR,"static")
+#]
+
+
+#MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+
+#django_heroku.settings(locals())
